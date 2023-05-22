a75ebd02805bd992edf22f73f8ca59408c455ba1
Mauricio Vargas <mavargas11@uc.cl>
2023-05-22 19:33:13 -0400

    build site with pkgdown

---
+++ R/r4dstutoriales-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @import datos
+#' @import dplyr
+#' @import ggplot2
+#' @import learnr
+#' @import pkgdown
+#' @import tidyr
+NULL
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
